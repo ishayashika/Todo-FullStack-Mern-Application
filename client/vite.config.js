b8ec004835897eb72f0c799691da578f32f8f3af
ishayashika <122111684+ishayashika@users.noreply.github.com>
2025-06-01 11:44:14 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'https://todo-fullstack-mern-application-server.onrender.com',
         changeOrigin: true,
         secure: false,
       },
