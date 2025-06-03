e45308e1b74faa14891c7ee664c4b05aecaeeaf6
ishayashika <122111684+ishayashika@users.noreply.github.com>
2025-06-03 13:58:20 +0530

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -9,9 +9,7 @@ import axios from 'axios'
 axios.defaults.withCredentials = true;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
     <Router>
       <App />
     </Router>
-  </React.StrictMode>,
 ); 
